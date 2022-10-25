88dc3d29b1f712917c2155d57c970ee694133c6d
Pedro Bustamante <pedrobustamante35@hotmail.com>
2022-10-25 18:39:14 -0500

    exe and BigList

---
+++ cmake-build-debug/CMakeFiles/QuickSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuickSort.dir/main.cpp.obj"
+  "QuickSort.exe"
+  "QuickSort.exe.manifest"
+  "QuickSort.lib"
+  "QuickSort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuickSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
